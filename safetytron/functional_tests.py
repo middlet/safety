772cf676ab6f8224e2ecf1a4c4321ceb4fa18315
lee <ljm@it-innovation.soton.ac.uk>
2013-07-27 08:35:00 +0100

    first functional test now using testing framework

--- safetytron/functional_tests.py
+++ safetytron/functional_tests.py
@@ -2,7 +2,31 @@
 
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:31337')
+import unittest
 
-assert 'Django' in browser.title
+# Feature: Compare two images of a city for safety
+#   In order to decide the safety of a city
+#   I want users to compare random images from google streetview
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_can_see_our_app(self):
+        """
+        Scenario: a user visits our website
+            Given: a visitor goes to the URL
+            When: they look at the title
+            Then: they see the application name
+        """
+        self.browser.get('http://localhost:31337')
+        self.assertIn('safetytron', self.browser.title)
+        self.fail("finish the tests")
+        
+if __name__ == '__main__':
+    unittest.main()
