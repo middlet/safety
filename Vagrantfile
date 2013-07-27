4dd128894729a55af35015402ae34ff33231ca4b
lee <ljm@it-innovation.soton.ac.uk>
2013-07-27 08:06:41 +0100

    first version of the template application

---
+++ Vagrantfile
@@ -0,0 +1,32 @@
+#
+# city safety 
+#
+
+# config options
+ip = '192.168.0.53'
+hostname = 'safetytron'
+ram = '512'
+
+# provision script
+$script = <<SCRIPT
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo pip install -r /vagrant/requirements.txt
+SCRIPT
+
+Vagrant.configure("2") do |config|
+    config.vm.box = 'precise32'
+    config.vm.box_url = 'http://files.vagrantup.com/precise32.box'
+    config.vm.hostname = hostname
+    config.vm.network :private_network, ip: ip
+    config.vm.network :forwarded_port, guest: 31337, host: 31337
+    
+    config.vm.provider :virtualbox do |vb|
+        vb.customize [
+            'modifyvm', :id,
+            '--name', hostname,
+            '--memory', ram
+        ]
+    end
+    config.vm.provision :shell, :inline => $script
+end
---
+++ fabfile.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from fabric.api import *
+from fabric.contrib import files
+
+# globals
+env.project_name = "safetytron"
+
+# environment
+def dev():
+    """
+    use the local development server
+    """
+    env.hosts = ['192.168.0.53']
+    # get vagrant key
+    result = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    env.key_filename = result.split()[1].strip('"')
+    env.user = 'vagrant'
+    env.code_root = '/vagrant/'+env.project_name
+    env.tile_root = '/vagrant'
+    
+# tasks
+def runserver(port=31337):
+    """
+    start the server running on the remote machine
+    """
+    with cd(env.code_root):
+        run('python ./manage.py runserver 0.0.0.0:%s' % port)
+        
+def syncdb():
+    """
+    run syncdb on the server
+    """
+    with cd(env.code_root):
+        run('python ./manage.py syncdb --noinput')
+        
+def resetdb():
+    """
+    flush the db and resync it
+    """
+    with cd(env.code_root):
+        run('python ./manage.py flush --noinput')
+        run('python ./manage.py syncdb --noinput')
+    
