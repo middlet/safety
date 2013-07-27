2c2192df0a3463992aeef9d86063d9a3df6778b1
lee <ljm@it-innovation.soton.ac.uk>
2013-07-27 08:44:16 +0100

    basic template for home page

--- safetytron/safety/tests.py
+++ safetytron/safety/tests.py
@@ -1,16 +1,13 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+these are the tests for the main application
 """
 
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from safety.views import home_page
 
+class HomePageTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
