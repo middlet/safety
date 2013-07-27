dae2f82813e91a396c39eec2a97c5b8127562cca
lee <ljm@it-innovation.soton.ac.uk>
2013-07-27 08:52:58 +0100

    basic view now returns simple html

--- safetytron/safety/views.py
+++ safetytron/safety/views.py
@@ -1,4 +1,4 @@
-# Create your views here.
+from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>safetytron</title></html>')
