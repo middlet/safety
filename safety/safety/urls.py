82a0dbba1c1769732424840b8db119bce638fb2c
lee <ljm@it-innovation.soton.ac.uk>
2014-06-27 09:58:51 +0100

    added default url

--- safety/safety/urls.py
+++ safety/safety/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^$', 'gamecompare.views.home', name='home'),
     # Examples:
     # url(r'^$', 'safety.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
