98e457a64c836f90413a5e5c386f9c691f69c545
lee <ljm@it-innovation.soton.ac.uk>
2014-06-27 16:11:10 +0100

    reworked the debug application

--- safety/gamecompare/views.py
+++ safety/gamecompare/views.py
@@ -6,14 +6,20 @@ def debug(request, place):
 	"""
 	render a page displaying the data for a city
 	"""
-	# get the points inside the southampton polygon
-	# get southampton polygon
+	# get the points inside the places polygon
 	soton = Cities.objects.get(name__icontains=place)
 	soton_points = [[pi[0],pi[1]] for pi in soton.geom.coords[0]]
 	# find points in the polygon
 	roads = Roads.objects.filter(geom__within=soton.geom)
 	roads_points = [[ri.geom.coords[0],ri.geom.coords[1]] for ri in roads]
 	# probably should pass this in via json
-	context = {'roads':roads_points, 'poly':soton_points, 'bbox':soton.geom.extent}
+	context = {'roads':roads_points, 'poly':soton_points, 'bbox':soton.geom.extent, 'place':soton.name}
 	#
-	return render(request, 'home.html', context)
+	return render(request, 'debug.html', context)
+
+
+def home(request):
+	"""
+	get the street view images and cache them for reuse later
+	"""
+	pass
